56c56dd91fb1d11c6f3bdc357d35b9f4c5a4a5b2
Mauricio Julio Rodrigo Lemus Guzmán <93178012+Rodlemus03@users.noreply.github.com>
2025-03-03 17:20:34 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -26,3 +26,19 @@ class TuringMachine:
             self.display_tape()
             return True  # Indica que la máquina sigue en ejecución
         return False  # No hay transición, STOP MANO
+    def run(self):
+        print("\n=== Inicio de la Máquina de Turing ===\n")
+        while self.state != self.accept_state and self.step():
+            pass
+        print("\n=== Fin de la Máquina de Turing ===\n")
+        result_unary = ''.join(self.tape).strip('B')
+        result_decimal = len(result_unary)  # UNARIO a DECIMAL
+        print(f"\nResultado en unario: {result_unary}")
+        print(f"Resultado en decimal (Fibonacci de {self.input_length}): {result_decimal}\n")
+        return result_decimal
+    
+    def display_tape(self):
+        tape_display = ''.join(
+            [f"\033[91m{self.tape[i]}\033[0m" if i == self.head else self.tape[i] for i in range(len(self.tape))]
+        )
+        print(f"State: {self.state}\nTape: [{tape_display}]\n")
