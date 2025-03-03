217c33ed2ebf173ef7817abeecd5af174b0b1190
Mauricio Julio Rodrigo Lemus Guzmán <93178012+Rodlemus03@users.noreply.github.com>
2025-03-03 17:20:09 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -14,3 +14,15 @@ class TuringMachine:
         self.transitions = transitions
         self.accept_state = accept_state
         self.input_length = len(tape)  # longitud de la entrada
+
+    def step(self):
+        symbol = self.tape[self.head]
+        if (self.state, symbol) in self.transitions:
+            new_symbol, move, new_state = self.transitions[(self.state, symbol)]
+            print(f"Transition: ({self.state}, {symbol}) → ({new_state}, {new_symbol}, {move})")
+            self.tape[self.head] = new_symbol
+            self.head += 1 if move == 'R' else -1 if move == 'L' else 0
+            self.state = new_state
+            self.display_tape()
+            return True  # Indica que la máquina sigue en ejecución
+        return False  # No hay transición, STOP MANO
